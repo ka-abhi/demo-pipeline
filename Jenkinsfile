4c27240be78e19242bf991a32f32ce21c779379d
ka-abhi <ka.abhishek@outlook.com>
2021-07-21 22:13:32 +0530

    tf code push

---
+++ Jenkinsfile
@@ -0,0 +1,10 @@
+pipeline{
+    agent any
+    stages{
+        stage('INIT'){
+            steps{
+                sh "terraform init"
+            }
+        }
+    }
+}
---
+++ vpc.tf
@@ -0,0 +1,7 @@
+resource "aws-vpc" "tfvpc" {
+    cidr_block = "${var.cidr}"
+    instance_tenancy = "${var.tenancy}"
+    tags = {
+        Name = "${terraform.workspace}-tf-vpc"
+    }
+}
